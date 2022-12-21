30f3bf83bf041c0c6f830110a50996692242231a
Fuzi18 <fuzesi.t2003@gmail.com>
2022-12-21 11:15:39 +0100

    start

---
+++ src/main/java/hu/petrik/etlap/Etel.java
@@ -0,0 +1,72 @@
+package hu.petrik.etlap;
+
+public class Etel {
+    private int id;
+    private String name;
+    private String category;
+    private int price;
+    private String desc;
+
+    public Etel(String name, String category, int price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Etel(int id, String name, String category, int price, String desc) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.desc = desc;
+    }
+
+    public Etel(String name, String category, int price, String desc) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
+
---
+++ src/main/java/hu/petrik/etlap/EtlapController.java
@@ -0,0 +1,12 @@
+package hu.petrik.etlap;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class EtlapController {
+    @FXML
+    private TableView<Etel> etlapTable;
+
+
+}
