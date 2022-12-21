11b5e643791e0bf1475fd46d1f89afa48cbd9366
Fuzi18 <fuzesi.t2003@gmail.com>
2022-12-21 12:36:31 +0100

    egyenlore ddig jutottam

--- src/main/java/hu/petrik/etlap/EtlapController.java
+++ src/main/java/hu/petrik/etlap/EtlapController.java
@@ -1,7 +1,12 @@
 package hu.petrik.etlap;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class EtlapController {
     @FXML
@@ -18,6 +23,60 @@ public class EtlapController {
     private TableColumn<Etel, String> categoryCol;
     @FXML
     private TableColumn<Etel, Integer> priceCol;
+    private EtlapDB db;
+
+    private Etel getSelectedFood() {
+        int selectedIndex = etlapTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex == -1) {
+            alert(Alert.AlertType.WARNING, "A táblázatból előbb válasszon ki egy ételt!", "");
+            return null;
+        }
+        return etlapTable.getSelectionModel().getSelectedItem();
+    }
+
+    private Object alert(Alert.AlertType warning, String s, String s1) {
+        return null;
+    }
+
+    private Etel getSelectedFoodNullNotMatter() {
+        int selectedIndex = etlapTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex == -1) {
+            return null;
+        }
+        return etlapTable.getSelectionModel().getSelectedItem();
+    }
+
+    @FXML
+    public void torlesClick(ActionEvent actionEvent) {
+        Etel selected = getSelectedFood();
+        if (selected == null) return;
+        Optional<ButtonType> optionalButtonType = (Optional<ButtonType>) alert(Alert.AlertType.CONFIRMATION, "Biztos, hogy törölni szeretné a kiválasztott ételt?", "");
+        if (optionalButtonType.isEmpty() || !(optionalButtonType.get().equals(ButtonType.OK)) && !(optionalButtonType.get().equals(ButtonType.YES))) {
+            return;
+        }
+        try {
+            if (db.deleteFood(selected.getId())) {
+                alert(Alert.AlertType.WARNING, "Sikeres törlés", "");
+                listview.getItems().clear();
+            } else {
+                alert(Alert.AlertType.WARNING, "Sikertelen törlés", "");
+            }
+            readFood();
+        } catch (SQLException e) {
+            sqlAlert(e);
+        }
+    }
+    private void readFood() throws SQLException {
+        List<Etel> dogs = db.readFood();
+        etlapTable.getItems().clear();
+        etlapTable.getItems().addAll(dogs);
+    }
+
+
+
+    private void sqlAlert(SQLException e) {
+        alert(Alert.AlertType.ERROR, "Hiba történt az adatbázis kapcsolat kialakításakor", e.getMessage());
+    }
 
 
 
--- src/main/java/hu/petrik/etlap/EtlapDB.java
+++ src/main/java/hu/petrik/etlap/EtlapDB.java
@@ -1,8 +1,8 @@
 package hu.petrik.etlap;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EtlapDB {
     Connection conn;
@@ -17,5 +17,38 @@ public class EtlapDB {
     public EtlapDB() throws SQLException {
         String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
         conn = DriverManager.getConnection(url, DB_USER, DB_PASS);
+    }
+    public List<Etel> readFood() throws SQLException {
+        List<Etel> foods = new ArrayList<>();
+        String sql = "SELECT * FROM etlap";
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(sql);
+        while (result.next()) {
+            int id = result.getInt("id");
+            String name = result.getString("nev");
+            String category = result.getString("kategoria");
+            int price = result.getInt("ar");
+            String desc = result.getString("leiras");
+            Etel food = new Etel(id, name, category, price, desc);
+            foods.add(food);
+        }
+        return foods;
+    }
+
+    public boolean createFood(Etel food) throws SQLException {
+        String sql = "INSERT INTO etlap(nev, leiras, ar, kategoria) VALUES (?,?,?,?)";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, food.getName());
+        stmt.setString(2, food.getDesc());
+        stmt.setInt(3, food.getPrice());
+        stmt.setString(4, food.getCategory());
+        return stmt.executeUpdate() > 0;
+    }
+
+    public boolean deleteFood(int id) throws SQLException {
+        String sql = "DELETE FROM etlap WHERE id = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+        return stmt.executeUpdate() > 0;
     }
 }
