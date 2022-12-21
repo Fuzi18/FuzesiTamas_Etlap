d7d586caa3cf37a272d5bc293c7eaf3f8d285982
Fuzi18 <fuzesi.t2003@gmail.com>
2022-12-21 11:25:47 +0100

    EtlapController

--- src/main/java/hu/petrik/etlap/EtlapController.java
+++ src/main/java/hu/petrik/etlap/EtlapController.java
@@ -1,12 +1,25 @@
 package hu.petrik.etlap;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 
 public class EtlapController {
     @FXML
     private TableView<Etel> etlapTable;
+    @FXML
+    private Button torlesButton;
+    @FXML
+    private TableColumn<Etel, String> nameCol;
+    @FXML
+    private ListView<String> listview;
+    @FXML
+    private Button ujEtelFelveteleButton;
+    @FXML
+    private TableColumn<Etel, String> categoryCol;
+    @FXML
+    private TableColumn<Etel, Integer> priceCol;
+
+
 
 
 }
