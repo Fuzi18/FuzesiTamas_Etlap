617ab2ba4f69b79451c695eb1b75593ed5779209
Fuzi18 <fuzesi.t2003@gmail.com>
2022-12-21 11:34:23 +0100

    Connection

---
+++ src/main/java/hu/petrik/etlap/EtlapDB.java
@@ -0,0 +1,21 @@
+package hu.petrik.etlap;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EtlapDB {
+    Connection conn;
+    public static String DB_DRIVER = "mysql";
+    public static String DB_HOST = "localhost";
+    public static String DB_PORT = "3306";
+    public static String DB_DBNAME = "etlapdb";
+    public static String DB_USER = "root";
+    public static String DB_PASS = "";
+
+
+    public EtlapDB() throws SQLException {
+        String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
+        conn = DriverManager.getConnection(url, DB_USER, DB_PASS);
+    }
+}
